dc54ac358cce19e0da2c7dd5961b03cf5d2d29fb
배영찬 <byou0070@gmail.com>
2026-06-16 12:35:35 +0900

    web 업로드 사전 작업

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -1,7 +1,10 @@
 class ApiConstants {
-  //local host
-  static const String baseUrl = "http://10.0.2.2:8000";
+  //local host (에뮬레이터)
+  // static const String baseUrl = "http://10.0.2.2:8000";
+
+  //web
+  // static const String baseUrl = "http://127.0.0.1:8000";
 
   //실제 디바이스에서 테스트 중이면 내 PC의 아이피로 변경
-  // static const String baseUrl = "http://172.16.231.166:8000";
+  static const String baseUrl = "http://172.16.237.23:8000";
 }
