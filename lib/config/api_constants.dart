cd3e6da2ed6d5fede4c8bce6c90f9f0b1ac32f0e
배영찬 <byou0070@gmail.com>
2026-06-18 15:45:18 +0900

    병합 작업후 오류 수정

--- lib/config/api_constants.dart
+++ lib/config/api_constants.dart
@@ -3,13 +3,8 @@ class ApiConstants {
   // static const String baseUrl = "http://10.0.2.2:8000";
 
   //web
-  static const String baseUrl = "http://127.0.0.1:8000";
+  // static const String baseUrl = "http://127.0.0.1:8000";
 
-<<<<<<< HEAD
   // 외부 도메인 접속 (공유기 포트 포워딩 8000번 적용)
   static const String baseUrl = "http://alpakit.kro.kr";
-=======
-  // //실제 디바이스에서 테스트 중이면 내 PC의 아이피로 변경
-  // static const String baseUrl = "http://172.16.237.23:8000";
->>>>>>> origin/new-ui
 }
